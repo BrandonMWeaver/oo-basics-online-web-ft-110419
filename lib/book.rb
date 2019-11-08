f1befa9275b13b1348338362aca80e3b46381133
Brandon Weaver <a7xncob@gmail.com>
2019-11-08 18:03:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
   attr_accessor :author
   attr_accessor :page_count
+  attr_accessor :genre
   
   def initialize(title)
     @this_title = title
@@ -8,5 +9,10 @@ class Book
   
   def title
     return @this_title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+    self.page_count += 1
   end
 end
